e05fc504bc4c02b13e4166038da2add16ee13799
Susan Davis <sd3693@gmail.com>
2022-12-07 13:58:03 -0500

    Better error messages

--- runtests.sh
+++ runtests.sh
@@ -1,22 +1,58 @@
 #!/bin/sh
 
+TEST="sectored grid"
+
 dgdebug -q sectored-tests.dg sectored-grid.dg testrunner.dg stdlib.dg \
-    | grep 'tests passed successfully' && \
+    | grep 'tests passed successfully'
+
+if [ "$?" -ne "0" ]; then
+   echo '  *** Some tests failed in' $TEST '! ***'
+   exit 1
+fi
+
+TEST="time"
+
 dgdebug -q time-tests.dg time.dg testrunner.dg stdlib.dg \
-    | grep 'tests passed successfully' && \
-dgdebug -q schema-tests.dg schema.dg sectored-grid.dg testrunner.dg stdlib.dg \
-    | grep 'tests passed successfully' && \
+     | grep 'tests passed successfully'
+
+if [ "$?" -ne "0" ]; then
+   echo '  *** Some tests failed in' $TEST '! ***'
+   exit 1
+fi
+
+TEST="schema"
+
+dgdebug -q schema-tests.dg schema.dg sectored-grid.dg testrunner.dg \
+	stdlib.dg \
+     | grep 'tests passed successfully'
+
+if [ "$?" -ne "0" ]; then
+   echo '  *** Some tests failed in' $TEST '! ***'
+   exit 1
+fi
+
+TEST="sensors"
+
 dgdebug -q sensor-tests.dg sensors.dg schema.dg sectored-grid.dg \
 	    testrunner.dg stdlib.dg \
-    | grep 'tests passed successfully' && \
-dgdebug -q maneuver-tests.dg maneuvering.dg schema.dg sectored-grid.dg \
+	| grep 'tests passed successfully'
+
+if [ "$?" -ne "0" ]; then
+   echo '  *** Some tests failed in' $TEST '! ***'
+   exit 1
+fi
+
+TEST="maneuver"
+
+(dgdebug -q maneuver-tests.dg maneuvering.dg schema.dg sectored-grid.dg \
 	    testrunner.dg stdlib.dg \
-    | grep 'tests passed successfully' && \
-dgdebug -q damage-tests.dg damage.dg schema.dg sectored-grid.dg \
-	    testrunner.dg stdlib.dg \
-    | grep 'tests passed successfully' && \
-echo '  *** All tests passed. ***' && \
+     | grep 'tests passed successfully')
+
+if [ "$?" -ne "0" ]; then
+   echo '  *** Some tests failed in' $TEST '! ***'
+   exit 1
+fi
+
+echo '  *** All tests passed. ***' &&
 exit 0
 
-echo '  *** Some tests failed! ***'
-exit 1
